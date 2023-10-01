cb018355f8bdfab427312b7b0504d089a63e2a07
tamilpp25 <tamilpp25@gmail.com>
2023-10-01 12:56:55 +0530

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,11 @@ export default function Home() {
       return (
         <div className="flex flex-col justify-center items-center gap-3 h-screen">
           <div className="flex flex-row items-center gap-3">
-            <Image src="/favicon.png" alt="logo" height={65} width={65} />
-            <h1 className="text-7xl font-bold">FileCDN</h1>
+            <Image src="/favicon.png" alt="logo" height={50} width={50} />
+            <h1 className="text-6xl font-bold">FileCDN</h1>
           </div>
-          <h2 className="text-lg">Host and share files easily!</h2>
-          <h1 className="text-center text-2xl">
+          <h2 className="text-lg text-zinc-900 dark:text-zinc-400">Host and share files easily!</h2>
+          <h1 className="text-center text-2xl text-zinc-900 dark:text-zinc-400">
             Authentication is required to access further content!
           </h1>
           <Button className="h-auto w-36 text-base" onClick={() => signIn()}>
