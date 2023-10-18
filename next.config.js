4f6f0eb7cd7cffa44e07f878f2a386c7bf60dcca
tamilpp25 <tamilpp25@gmail.com>
2023-10-18 17:44:40 +0530

    Revert "add cors"
    This reverts commit bc62010c2825b58c450da4ac2fddbf27c9cf6879.

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${NEXTAUTH_URL}/:path*`,
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
